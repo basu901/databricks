26c6e1ad07404f20b010a6745c326e5944af1cbd
basu901 <basu901>
2024-11-17 03:19:50 +0000

    sql tables on Databricks

--- Formula_One_Project/sql_test/sql_object_demo.sql
+++ Formula_One_Project/sql_test/sql_object_demo.sql
@@ -34,18 +34,51 @@ SHOW TABLES in demo
 
 -- COMMAND ----------
 
+-- MAGIC %run "/Workspace/Users/shaunak.basu@perficient.com/formula_one_project/Formula_One_Project/includes/configuration"
 
+-- COMMAND ----------
+
+-- MAGIC %md 
+-- MAGIC #####Managed Tables#####
 
 -- COMMAND ----------
 
 -- MAGIC %python
 -- MAGIC
--- MAGIC #Creating a Managed Table
+-- MAGIC data_path = presentation_folder_path
 -- MAGIC
+-- MAGIC #Creating a Managed Table
+-- MAGIC race_data = spark.read.parquet(f"{data_path}/races_results")
+-- MAGIC race_data.write.format('parquet').saveAsTable('demo.race_results_python')
 
 -- COMMAND ----------
 
---Creating a Managed Table
+--You can use the database name in the namespace of the table for view, even if the current database is different
+
+DESCRIBE EXTENDED demo.race_results_python
+
+-- COMMAND ----------
+
 USE demo;
 
-CREATE TABLE IF NOT EXISTS 
+SHOW TABLES
+
+-- COMMAND ----------
+
+DESCRIBE EXTENDED race_results_python
+
+-- COMMAND ----------
+
+SELECT * FROM demo.race_results_python
+ORDER BY points DESC
+
+-- COMMAND ----------
+
+CREATE TABLE demo.race_results_sql
+AS
+SELECT * FROM demo.race_results_python
+WHERE race_year=2020
+
+-- COMMAND ----------
+
+SHOW TABLES in demo
